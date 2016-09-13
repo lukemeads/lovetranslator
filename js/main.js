2db0df87df5fb6c9671e5bbca0ebfd56b435cfc0
Luke <ballsackluke3395@gmail.com>
2016-09-13 22:59:50 +1200

    Converted JS to TypeScript and fixed progress bar displays.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 var sendBtn = $("#sendBtn");
 var pagetext = $("#page-text")[0];
 var textBox = $("#txtMessage")[0];
-var messageObj: AnalysedMessage;
-let progBarVisible : string = "";
-
+var messageObj;
+var progBarVisible = "";
 toggleProgBar("success"); // Dirty fix, the elements seem to not be initializing their display css property to none from style.css
 toggleProgBar("success");
 toggleProgBar("warning");
 toggleProgBar("warning");
 toggleProgBar("danger");
-toggleProgBar("danger"); 
-
-
+toggleProgBar("danger");
 sendBtn.on("click", function () {
     swal({
         title: "Analysing",
@@ -19,14 +16,13 @@ sendBtn.on("click", function () {
         timer: 2000,
         showConfirmButton: false
     });
-    let message: string = formatData(textBox.value);
+    var message = formatData(textBox.value);
     sendData(message, function (sentiment) {
         messageObj = new AnalysedMessage(textBox.value, sentiment);
         updateUI(messageObj);
     });
 });
-
-function sendData(message: string, callback): void {
+function sendData(message, callback) {
     $.ajax({
         url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
         beforeSend: function (xhrObj) {
@@ -40,65 +36,65 @@ function sendData(message: string, callback): void {
         processData: false
     })
         .done(function (data) {
-            if (data.length != 0) {
-                var score = data.documents[0].score;
-                callback(score); // Success
-            } else {
-                pagetext.innerHTML = "Error";
-            }
-        })
+        if (data.length != 0) {
+            var score = data.documents[0].score;
+            callback(score); // Success
+        }
+        else {
+            pagetext.innerHTML = "Error";
+        }
+    })
         .fail(function (error) {
-            pagetext.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
-            console.log(error.getAllResponseHeaders());
-        });
+        pagetext.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
+        console.log(error.getAllResponseHeaders());
+    });
 }
-
-function updateUI(obj : AnalysedMessage): void {
-     if (progBarVisible != "") {
+function updateUI(obj) {
+    if (progBarVisible != "") {
         toggleProgBar(progBarVisible);
     }
     pagetext.innerHTML = "Sentiment:";
-    let percent: number = obj.sentiment * 100;
-    let colour: string = getProgressBarColour(obj.sentiment);
+    var percent = obj.sentiment * 100;
+    var colour = getProgressBarColour(obj.sentiment);
     var progBar = $("#prog-bar-" + colour);
     progBar[0].style.width = percent + "%";
     progBar[0].innerHTML = percent + "%";
     toggleProgBar(colour);
-     progBarVisible = colour;
+    progBarVisible = colour;
 }
-
-function toggleProgBar(colour: string): void {
-    let bar: Node = document.getElementById('prog-bar-' + colour).parentNode;
+function toggleProgBar(colour) {
+    var bar = document.getElementById('prog-bar-' + colour).parentNode;
     if (!(bar.style.display) || bar.style.display === 'none') {
         bar.style.display = 'block';
-    } else {
+    }
+    else {
         bar.style.display = 'none';
     }
 }
-
-function getProgressBarColour(sentiment: number): string {
+function getProgressBarColour(sentiment) {
     if (sentiment < 0.2) {
         return "danger";
-    } else if ((sentiment > 0.2) && (sentiment < 0.6)) {
+    }
+    else if ((sentiment > 0.2) && (sentiment < 0.6)) {
         return "warning";
-    } else {
+    }
+    else {
         return "success";
     }
 }
-
-function formatData(text: string): string { // This is the format the API requires
+function formatData(text) {
     return "{\"documents\": [{"
         + "\"language\": \"en\","
         + "\"id\": \"1\","
         + "\"text\": \"" + text + "\"}]}";
 }
-
 // Future fields can be added: emoji count, key phrases, language detection
-class AnalysedMessage {
-    text: string; // raw text given by user
-    sentiment: number; // sentiment from API
-    constructor(public text, public sentiment) {
+var AnalysedMessage = (function () {
+    function AnalysedMessage(text, sentiment) {
+        this.text = text;
+        this.sentiment = sentiment;
         this.text = text;
         this.sentiment = sentiment;
     }
-}
+    return AnalysedMessage;
+}());
